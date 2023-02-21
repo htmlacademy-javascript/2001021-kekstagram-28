02b443e698aefe5e114b495b553791f436ee6be6
Eduard Osmanov <osmanov48@gmail.com>
2023-02-21 13:33:20 +0300

    change double quotes to single quotes

--- js/function.js
+++ js/function.js
@@ -6,42 +6,41 @@ function getStringLength(str, val) {
   }
   return false;
 }
-getStringLength("проверяемая строка", 20); // true
-getStringLength("проверяемая строка", 18); // true
-getStringLength("проверяемая строка", 10); // false
+getStringLength('проверяемая строка', 20); // true
+getStringLength('проверяемая строка', 18); // true
+getStringLength('проверяемая строка', 10); // false
 
 // Функция для проверки, является ли строка палиндромом
 function chekedPalindrom(str) {
   // Переводим в нижний регистр и удаляем все лишние отступы
-  let strToLower = str.toLowerCase().trim();
-  return strToLower == strToLower.split("").reverse().join("");
+  const strToLower = str.toLowerCase().trim();
+  return strToLower === strToLower.split('').reverse().join('');
 }
 
-chekedPalindrom("ДовОд "); // true
-chekedPalindrom("топот"); // true
-chekedPalindrom("Кекс"); // false
+chekedPalindrom('ДовОд '); // true
+chekedPalindrom('топот'); // true
+chekedPalindrom('Кекс'); // false
 
 function checkNewPalindrom(str) {
-  let strToLower = str.toLowerCase().trim();
-  let res = "";
+  const strToLower = str.toLowerCase().trim();
+  let res = '';
   for (let i = strToLower.length - 1; i >= 0; --i) {
     res += strToLower[i];
-    console.log(res);
   }
-  return res == strToLower.split("").reverse().join("");
+  return res === strToLower.split('').reverse().join('');
 }
-checkNewPalindrom("Лёша на полке клопа нашёл "); // true
+checkNewPalindrom('Лёша на полке клопа нашёл '); // true
 
 /* Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа */
 function extractNumberOfString(value) {
-  let num = "";
+  let num = '';
   // Если приходит число
-  if (typeof value === "number") {
+  if (typeof value === 'number') {
     return value;
   }
   // Если приходит строка, обходим ее циклом
   for (let i = 0; i < value.length; i++) {
-    const isInt = parseInt(value[i]);
+    const isInt = parseInt(value[i], 10);
     // Если число, то присваиваем значение переменной num
     if (isInt || isInt === 0) {
       num += value[i];
@@ -53,10 +52,10 @@ function extractNumberOfString(value) {
   }
   return num;
 }
-extractNumberOfString("а я томат");
-extractNumberOfString("2023 год");
-extractNumberOfString("ECMAScript 2022");
-extractNumberOfString("1 кефир, 0.5 батона");
+extractNumberOfString('а я томат');
+extractNumberOfString('2023 год');
+extractNumberOfString('ECMAScript 2022');
+extractNumberOfString('1 кефир, 0.5 батона');
 extractNumberOfString(2030);
 extractNumberOfString(-1);
 
@@ -66,7 +65,7 @@ function padStart(str, len, padstr) {
   if (str.length > len) {
     return str;
   }
-  let subStr = "";
+  let subStr = '';
   /* Пока subStr меньше заданой длины, проходим циклом и присваеваем значение в переменную subStr */
   while (subStr.length < len) {
     subStr += padstr;
@@ -75,9 +74,9 @@ function padStart(str, len, padstr) {
   /* Если строка слишком длинная обрезаем с конца и конкатенируем с исходной строкой */
   return subStr.slice(0, len - str.length) + str;
 }
-padStart("1", 2, "0");
-padStart("1", 4, "0");
-padStart("q", 4, "werty");
-padStart("q", 4, "we");
-padStart("qwerty", 4, "0");
-padStart("qwerty", 3, "0");
+padStart('1', 2, '0');
+padStart('1', 4, '0');
+padStart('q', 4, 'werty');
+padStart('q', 4, 'we');
+padStart('qwerty', 4, '0');
+padStart('qwerty', 3, '0');
