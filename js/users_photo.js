7da7becc27c387f2113750d28b9883a6ecad71ab
edmondev80 <osmanov48@gmail.com>
2023-03-12 17:46:54 +0300

    add new function clonePictureElement

--- js/users_photo.js
+++ js/users_photo.js
@@ -2,16 +2,18 @@
 const picturesContainer = document.querySelector('.pictures');
 const pictureUsersTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const samePictureFragment = document.createDocumentFragment();
-const similarPictures = (element) => {
-  element.forEach(({url, likes, comments}) => {
-    const pictureElement = pictureUsersTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    samePictureFragment.append(pictureElement);
-    picturesContainer.appendChild(pictureElement);
-  });
+const clonePictureElement = ({url, likes, comments}) => {
+  const pictureElement = pictureUsersTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  picturesContainer.append(pictureElement);
 };
 
-export{similarPictures};
+const makeSimilarPictures = (element) => {
+  const pictureFragment = document.createDocumentFragment();
+  element.forEach(clonePictureElement);
+  pictureFragment.append(element);
+};
+
+export{makeSimilarPictures};
