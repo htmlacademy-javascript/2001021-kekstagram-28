0fa17e83992b79f37706a167ce6dbf36f8c6e6d2
edmondev80 <osmanov48@gmail.com>
2023-03-23 23:24:16 +0300

    added function onPicturesClick

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 import {createPhotos} from './data.js';
 import {renderPictures} from './users_photo.js';
+import {openBigPicture} from './big_picture.js';
+import {picturesContainer} from './users_photo.js';
 
 renderPictures(createPhotos());
+
+const onPicturesClick = (evt) => {
+  if (evt.target.closest('.picture')) {
+    const element = evt.target.closest('.picture');
+    const arrayIndex = element.dataset.id;
+
+    openBigPicture(arrayIndex);
+  }
+};
+
+
+picturesContainer.addEventListener('click', onPicturesClick);
